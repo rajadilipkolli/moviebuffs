94e6457a2fd66e2b559a8283995d37cc8bc18fce
rajadileepkolli <rajadileepkolli@gmail.com>
2021-12-29 11:37:44 +0530

    upgrade react-scripts to 4.0.0

--- moviebuffs/src/test/java/com/sivalabs/moviebuffs/web/controller/MovieControllerIT.java
+++ moviebuffs/src/test/java/com/sivalabs/moviebuffs/web/controller/MovieControllerIT.java
@@ -1,18 +1,12 @@
 package com.sivalabs.moviebuffs.web.controller;
 
 import com.sivalabs.moviebuffs.common.AbstractIntegrationTest;
-import com.sivalabs.moviebuffs.core.service.MovieService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class MovieControllerIT extends AbstractIntegrationTest {
-
-	@Autowired
-	private MovieService movieService;
 
 	@BeforeEach
 	void setUp() {
