8180b5726ccb125a0140cab851858b073d8069c1
Raja Kolli <rajadilipkolli@gmail.com>
2025-06-28 11:40:16 +0000

    style: format main method in TestMovieBuffsApplication for consistency

--- moviebuffs-service/src/test/java/com/sivalabs/moviebuffs/TestMovieBuffsApplication.java
+++ moviebuffs-service/src/test/java/com/sivalabs/moviebuffs/TestMovieBuffsApplication.java
@@ -7,9 +7,8 @@ import com.sivalabs.moviebuffs.common.TestConfig;
 
 public class TestMovieBuffsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.from(MovieBuffsApplication::main)
-                .with(TestConfig.class, PostGreSQLContainer.class)
-                .run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.from(MovieBuffsApplication::main).with(TestConfig.class, PostGreSQLContainer.class).run(args);
+	}
+
 }
